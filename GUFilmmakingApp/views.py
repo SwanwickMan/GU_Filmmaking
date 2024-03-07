d6bb6df50152e02b13fe252ba6c1b9ded89270ad
Andrew Laing <2775878l@student.gla.ac.uk>
2024-03-07 22:02:10 +0000

    Implemented skeletons of views

--- GUFilmmakingApp/views.py
+++ GUFilmmakingApp/views.py
@@ -1,3 +1,106 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    context_dict = {}
+    response = render(request, 'base.html', context=context_dict)
+
+    return response
+
+
+def search(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'signup.html')
+
+
+def most_viewed(request):
+    context_dict = {}
+    response = render(request, 'most_viewed.html', context=context_dict)
+
+    return response
+
+
+def most_liked(request):
+    context_dict = {}
+    response = render(request, 'most_liked.html', context=context_dict)
+
+    return response
+
+
+def profile(request):
+    context_dict = {}
+    response = render(request, 'profile.html', context=context_dict)
+
+    return response
+
+
+def user_liked(request):
+    context_dict = {}
+    response = render(request, 'user_liked.html', context=context_dict)
+
+    return response
+
+
+def user_posts(request):
+    context_dict = {}
+    response = render(request, 'user_posts.html', context=context_dict)
+
+    return response
+
+
+def categories(request):
+    context_dict = {}
+    response = render(request, 'categories.html', context=context_dict)
+
+    return response
+
+
+#implement slugs later
+def long_movies(request, category_name_slug):
+    context_dict = {}
+    response = render(request, 'long_movies.html', context=context_dict)
+
+    return response
+
+
+def short_movies(request, category_name_slug):
+    context_dict = {}
+    response = render(request, 'short_movies.html', context=context_dict)
+
+    return response
+
+
+def posters(request, category_name_slug):
+    context_dict = {}
+    response = render(request, 'posters.html', context=context_dict)
+
+    return response
+
+
+def behind_the_scenes(request, category_name_slug):
+    context_dict = {}
+    response = render(request, 'behind_the_scenes.html', context=context_dict)
+
+    return response
+
+
+def user_login(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'login.html')
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'signup.html')
+
+
+
+
+

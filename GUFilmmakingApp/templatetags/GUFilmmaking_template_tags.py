e70fc2e7801c1becf87fafc6707eaa45977fbdf4
“Sophia <2764815b@student.gla.ac.uk>
2024-03-15 22:50:44 +0000

    cookies, home.html, templatetags, home.css

--- GUFilmmakingApp/templatetags/GUFilmmaking_template_tags.py
+++ GUFilmmakingApp/templatetags/GUFilmmaking_template_tags.py
@@ -1,4 +1,8 @@
 from django import template
+from GUFilmmakingApp.models import Post
 
 register = template.Library()
 
+@register.simple_tag
+def get_most_viewed_posts(count=3):
+    return Post.objects.order_by('-views')[:count]
--- GUFilmmakingApp/views.py
+++ GUFilmmakingApp/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from GUFilmmakingApp.forms import PosterForm, MovieForm, BTSForm
 from django.shortcuts import redirect
 from django.urls import reverse
-
+from datetime import datetime
+from GUFilmmakingApp.models import Category
 
 # Create your views here.
 def index(request):
+    category_list = Category.objects.order_by('-views')[:3]
     context_dict = {}
-    response = render(request, 'base.html', context=context_dict)
-
+    context_dict['category'] = category_list
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'home.html', context=context_dict)
     return response
 
 
@@ -135,6 +139,21 @@ def user_signup(request):
         return render(request, 'signup.html')
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
 
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,'last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],'%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+        request.session['visits'] = visits
 
 

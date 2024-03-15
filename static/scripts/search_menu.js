f6eaf613d5b21dc746eea5c504e3162dd33325a0
Andrew Laing <2775878l@student.gla.ac.uk>
2024-03-15 13:38:23 +0000

    even further implemented search page

--- static/scripts/search_menu.js
+++ static/scripts/search_menu.js
@@ -4,7 +4,7 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    var content = document.getElementById("content");
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {

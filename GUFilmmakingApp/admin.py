6f74fac466a84744ef7a47056a1d93fb07f39504
Porter14x <2773303P@student.gla.ac.uk>
2024-03-20 16:35:17 +0000

    deleted Category stuuf wasn't being used, some tests as well not all working

--- GUFilmmakingApp/admin.py
+++ GUFilmmakingApp/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from GUFilmmakingApp.models import Category, Post, UserProfile
+from GUFilmmakingApp.models import Post, UserProfile
 
-admin.site.register(Category)
 admin.site.register(Post)
 admin.site.register(UserProfile)

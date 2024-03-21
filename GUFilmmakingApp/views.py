10227c47e42153c83f532c5f88a948c7550c60b0
Porter14x <2773303P@student.gla.ac.uk>
2024-03-21 14:07:55 +0000

    fixed merge issues

--- GUFilmmakingApp/views.py
+++ GUFilmmakingApp/views.py
@@ -61,7 +61,9 @@ def profile(request, content_name_slug):
 
     context_dict = {"profile": user_profile,
                     "profile_pic_form": profile_pic_form,
-                    "bio_form": bio_form}
+                    "bio_form": bio_form,
+                    "profile_posts" : profile_posts,
+                    "liked_posts" : liked_posts}
     return render(request, 'GUFilmmakingApp/profile.html', context=context_dict)
 
 
